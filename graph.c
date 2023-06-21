1d2f6255413bfdc4a3646e4ab980f9f93e844e09
haraldtv <harald.tverdal@protonmail.com>
2023-06-21 12:54:56 +0200

    Update graph.c

--- graph.c
+++ graph.c
@@ -29,7 +29,7 @@ void plotGraph(dataPoints inputData) {
     //Need a function to normalize and scale the datapoints from inputData
 
     //Prints the data
-    //WARNING! Input not scaled to size, and will most likely break
+    //WARNING! Input not scaled to size, and will most likely break (most likely as in absolutely)
     for (i=0; i<inputData.size; i++) {
         mvaddch(inputData.y[i], inputData.x[i], '*');
     }
@@ -38,4 +38,47 @@ void plotGraph(dataPoints inputData) {
     getch();
     endwin();
     
+}
+
+//This function needs to normalize the datapoints between 0 and 1
+//formula: z = (x-min) / (max-min)
+//Need the min and max values from the dataset
+void normalizeData(dataPoints *inp) {
+    int i;
+    double minX, minY, maxX, maxY;
+
+    minX = minValue(inp->size, inp->x);
+    minY = minValue(inp->size, inp->y);
+
+    maxX = maxValue(inp->size, inp->x);
+    maxY = maxValue(inp->size, inp->y);
+
+    for (i=0; i < inp->size; i++) {
+        inp->y[i] = 
+    }
+}
+
+//Unsure if i want the function to take a dataset as input, or hardcode it to return min value for both inp.x and inp.y
+double minValue(int size, double *datalist) {
+    int i;
+    double temp;
+    temp = datalist[0];
+    for (i=0; i<size; i++) {
+        if (temp > datalist[i]) {
+            temp == datalist[i];
+        }
+    }
+    return temp;
+}
+
+double maxValue(int size, double *datalist) {
+    int i;
+    double temp;
+    temp = datalist[0];
+    for (i=0; i<size; i++) {
+        if (temp < datalist[i]) {
+            temp == datalist[i];
+        }
+    }
+    return temp;
 }
